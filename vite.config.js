1dceda4f6f7d8c934ef5b7bca40fe53395b7ad0b
realARSIK <kahalinarsenii@gmail.com>
2025-06-02 09:50:16 +0500

    ci: add GitHub Pages deploy workflow, update base to '/gte-landing/', and install gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/gte-landing/',
   root: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'public'),
   build: {
